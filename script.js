8db12a904dd7b265757f3ebae8d31076b372407d
dafahfabiansyah <davarobiansyah@gmail.com>
2023-05-29 15:35:22 +0700

    menambahkan product

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// student
 const student = {
   name: 'dafah fabian syah',
   age: 18,
   school: 'Bintang Nusantara School',
-  //   majors: 'teknik komputer dan jaringan',
+  //   majors: 'teknik komputer dan jaringan', alias
 };
 function printMurid({ name, age, majors = 'TKJ', school }) {
   console.log(name, age, majors, school);
 }
 printMurid(student);
+
+// product
+const product = {
+  type: 'makanan',
+  //   price: 2000, alias
+  price: 'dua ribu rupiah',
+  halal: true,
+};
+
+function printProduct({ type, price, halal }) {
+  console.log(type, price, halal);
+}
+printProduct(product);
